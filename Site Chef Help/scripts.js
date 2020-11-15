fb86ebe6c4dcd7585f7e05d43ccf1efff2db4d2e
Yuri-Cruz <yuri.cruz@bandtec.com.br>
2020-11-14 22:04:27 -0300

    index completo!

---
+++ Site Chef Help/scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+    
+    $('.pratos').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplayspeed: 3000,
+        arrows: false,
+        nextArrow: null,
+        prevArrow: null 
+      }) 
+      
+})
